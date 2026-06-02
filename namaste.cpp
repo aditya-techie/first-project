058ad8753380998683c8d7b5b7abf57cdc3d199c
aditya-techie <pratapaditya2200@gmail.com>
2026-06-02 12:25:58 +0530

    first commit

---
+++ namaste.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"namaste dunia" <<endl;
+    cout<<"I am vanshuu, persuing B.Tech from RBSETC" ;
+
+    
+}
---
+++ second.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+/*    char c = 'a';
+cout << c <<endl;
+
+int i = 998981;
+cout<< i <<endl;
+
+bool bl = true;
+cout<< bl << endl;
+
+float f = 1.2;
+cout<< f <<endl;
+
+double d = 1.232;
+cout<< d <<endl;
+
+int size = sizeof(i);
+cout<< "size of i is = " << size << endl;
+
+
+int b = 'b';
+cout << b << endl;
+
+char ch = 100;
+cout<< ch <<endl;
+
+unsigned int a = -112;
+cout << a << endl;
+
+double a = 2.0/5;
+cout << a << endl;
+cout<< "2.00/5" <<endl;
+*/
+
+int a = 2;
+int b = 4;
+
+cout << "1 for true" <<endl;
+cout << "0 for false" <<endl;
+bool first = (a==b);
+cout << first <<endl;
+
+bool second = (a!=b);
+cout << second <<endl;
+
+bool third = (a<b);
+cout << third <<endl;
+
+bool fourth = (a>b);
+cout << fourth <<endl;
+
+bool fifth = (a<=b);
+cout << fifth <<endl;
+
+bool sixth = (a>=b);
+cout << sixth <<endl; 
+
+}
